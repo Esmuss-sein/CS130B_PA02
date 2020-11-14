882af848c93116a7e222af11b364797dd58946ca
Yu Yao <yu_yao@ucsb.edu>
2020-11-14 09:50:52 +0800

    <>

--- Seed.cpp
+++ Seed.cpp
@@ -13,20 +13,52 @@ using namespace std;
 
 int main(){
 	ios::sync_with_stdio(false);
-
+	//Declare variables and read n;
 	int n,w_1,w_2,L,R;
 	scanf("%d", &n);
 
 	L = 1;
 	R = n;
-
+	//Find within 16 times.
 	for(int i = 0; i < 16; i++){
+		//find span (range) of possible trees.
 		int span = R - L + 1;
-		int p_0 = L, p_25 = L+span/4, p_50 = L + span/2, p_75 = L + 3*span/4, p_1 = R;
-		
+		if(span == 2){
+			printf("Q %d %d %d %d \n", L,L,R,R);
+			cout.flash();
+			scanf("%d %d", &w_1,&w_2);
+
+			if(w_1 == 1){
+				printf("A %d\n", L);
+			}
+			else{
+				printf("A %d\n",R);
+			}
+			cout.flush();
+			break;
+		}//If only two trees, no need more observatiion. Break the loop and cout solution.
+		else if(span == 3){
+			printf("Q %d %d %d %d", L,L,L+1,L+1);
+			cout.flush();
+			scanf("%d %d", &w_1,&w_2);
+			if(w_1 == 1){
+				printf("A %d\n", L);
+			}
+			else if(w_2 == 1){
+				printf("A %d\n", L+1);
+			}
+			else{
+				printf("A %d\n", R)
+			}
+			cout.flush();
+			break;
+		}//Similar if span is 3. 
+
+		int p_0 = L, p_25 = L+span/4, p_50 = L + span/2, p_75 = L + 3*span/4, p_1 = R;//Set the points on 1/4, 1/2, 3/4, which divide the span into four equal parts. 
+
 		printf("Q %d %d %d %d\n", p_0, p_50-1, p_25, p_75 - 1);
 		cout.flush();
-		
+
 		scanf("%d,%d",&w_1,&w_2);
 		if(w_1 == 0 && w_2 == 0){
 			L = p_75;
