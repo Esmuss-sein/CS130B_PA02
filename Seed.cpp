92d73378767203eb5a0d720fca8a92fe2998d9db
Yu Yao <yu_yao@ucsb.edu>
2020-11-14 11:06:43 +0800

    <>

--- Seed.cpp
+++ Seed.cpp
@@ -56,23 +56,23 @@ int main(){
 		else{
 			int p_0 = L, p_25 = L+span/4, p_50 = L + span/2, p_75 = L + span/2+span/4, p_1 = R;//Set the points on 1/4, 1/2, 3/4, which divide the span into four equal parts. 
 
-			printf("Q %d %d %d %d\n", p_0, p_50-1, p_25, p_75 - 1);
+			printf("Q %d %d %d %d\n", p_0, p_50, p_25, p_75);
 			cout.flush();
 
 			scanf("%d %d",&w_1,&w_2);
 			if(w_1 == 0 && w_2 == 0){
-				L = p_75;
+				L = p_75-1;
 			}
 			else if(w_1 == 0 && w_2 == 1){
-				L = p_50;
-				R = p_75;
+				L = p_50-1;
+				R = p_75+1;
 			}
 			else if(w_1 == 1 && w_2 == 0){
-				R = p_50;
+				R = p_50-1;
 			}
 			else if(w_1 == 1 && w_2 == 1){
-				L = p_25;
-				R = p_50;
+				L = p_25-1;
+				R = p_50+1;
 			}
 		}
 
